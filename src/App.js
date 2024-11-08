1794a246bb40744c5de204f6a03bbdfb00d7a18b
SaChuma <104769467+SaChuma@users.noreply.github.com>
2024-11-08 18:09:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./Components/Content";
+import Home from "./Components/Home";
+import Loginform from "./Components/Loginform";
+import Privacy from "./Components/Privacy";
+import Signup from "./Components/Signup";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Router>
+      <Navbar/>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/privacy" element={<Privacy/>}/>
+        <Route path="/loginform" element={<Loginform/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+
+       </Routes>
+
+    </Router>
+  </div>
   );
-}
+};
 
 export default App;
